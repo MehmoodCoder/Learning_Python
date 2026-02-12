35b542f1034240d6744543dcebe7c2776a35e6f7
Mehmood Coder <mehmood.mh@gmail.com>
2026-02-12 20:54:34 +0500

    This is my first python task

---
+++ Learning_python/first_task.py
@@ -0,0 +1,7 @@
+no = int(input("Enter your mask : "))
+if no <= 40:
+    print("Afsos! Aap Fail hain.")
+elif no > 40 and no <= 100:
+    print("Mubarak ho! Aap Pass hain.")
+else:
+    print("Invalid!")    
