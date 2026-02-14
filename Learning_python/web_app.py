5b29706aa8cb715f0e3fffd31eeeb9d230efab1a
Mehmood Coder <mehmood.mh@gmail.com>
2026-02-14 19:16:00 +0500

    This is my first Web app in python with the help of Ai

---
+++ Learning_python/web_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+# Website ka Title
+st.title("Mehmood's first Web App 🚀")
+st.title("Python Powar!")
+
+# Text likhne ke liye
+st.write("Welcome to my Python Frontend!")
+
+# User se Input lena
+naam = st.text_input("Enter your name :")
+
+# Agar naam likha jaye to button dikhayein
+if st.button('Click'):
+    st.balloons() # Ek mazy dar effect
+    st.success(f"Assalam-o-Alaikum {naam}!")
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Mehmood's Data Dashboard 📊")
+
+# Ek sidebar banate hain
+st.sidebar.header("Settings")
+user_input = st.sidebar.slider("Select No ", 10, 100, 50)
+
+st.write(f"You selected {user_input} points.")
+
+# Random data banate hain chart ke liye
+chart_data = pd.DataFrame(
+    np.random.randn(user_input, 2),
+    columns=['A', 'B']
+)
+
+# Chart ko screen par dikhayein
+st.line_chart(chart_data)
+
+if st.button('Magic !'):
+    st.snow() # Is baar barf giregi!
+
+
+    #python3 -m streamlit run file_path
