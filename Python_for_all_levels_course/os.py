360926758e123d3fd8b7e36f619cb220fa82fa9e
Mehmood Coder <mehmood.mh@gmail.com>
2026-05-07 20:00:59 +0500

    This is the code of os library in python.

--- Python_for_all_levels_course/os.py
+++ Python_for_all_levels_course/os.py
@@ -1,5 +1,5 @@
 import os
-list1 = os.walk("C:\\Users\\Mehmood\\Desktop\\Python for all levels course") # it is used to get the list of all files and directories in a directory and its subdirectories.
+list1 = os.walk("Python_for_all_levels_course") # it is used to get the list of all files and directories in a directory and its subdirectories.
 
 
 
@@ -15,4 +15,36 @@ for root, dirs, files in list1:
         print(i)
     for file in files:
         print(file) 
+
+
+# 2.
+def li_dir(s):
+    def dir_list(d):
+        nonlocal tab_stop
+        files = os.listdir(d)
+        for f in files:
+            current_dir = os.path.join(d,f)
+            if os.path.isdir(current_dir):
+                print("\t"*tab_stop + " Directory "+ f)
+                tab_stop += 1
+                dir_list(current_dir)
+                tab_stop -= 1
+            else:
+                print(f)
+                
+    tab_stop=0
+    
+    if os.path.exists(s):
+        print("Directory Listing of " + s)
+        dir_list(s)
+        
+    else:
+        print(s+ " Directory does not exist!")
+
+
+li_dir("Python_for_all_levels_course")
+        
+
+
+        
 
