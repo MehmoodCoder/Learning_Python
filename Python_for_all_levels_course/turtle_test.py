74f02d6cfcc054728937680e79b0203d8a202969
Mehmood Coder <mehmood.mh@gmail.com>
2026-05-14 12:46:29 +0500

    This is the demo code of turtle library in python.

---
+++ Python_for_all_levels_course/turtle_test.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+color("red","blue")
+begin_fill()
+left(140)
+forward(111.65)
+for i in range(200):
+    right(1)
+    forward(1)
+left(120)
+for i in range(200):
+    right(1)
+    forward(1)
+    
+forward(111.65)
+end_fill()
+done()
+
+# import turtle
+
+# screen = turtle.Screen()
+# screen.setup(width=800, height=600)
+# t = turtle.Turtle()
+
+# t.shape("turtle")
+# t.color("blue") # Check karein yahan error aata hai ya nahi
+# t.forward(100)
+
+# turtle.done()
+
