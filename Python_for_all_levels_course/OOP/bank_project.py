e696c032ee7769e25ece7fdcc1d902e19d69a617
Mehmood Coder <mehmood.mh@gmail.com>
2026-05-14 14:57:15 +0500

    This code is about class example (bank) in python. and last comment is wrong.

--- Python_for_all_levels_course/OOP/bank_project.py
+++ Python_for_all_levels_course/OOP/bank_project.py
@@ -35,5 +35,31 @@ Ali.widraw(100000)
 # 2
 
 
+class bank():
+    
+    def __init__(self,name,balance):
+        self.name = name
+        self.balance = balance
+        print("Account Successfully Created")
+    def deposite(self,amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"You deposite {amount}")
+            self.show()
+    def widraw(self,amount):
+        if amount > 0:
+            self.balance -= amount 
+            print(f"You widraw {amount}")
+            self.show()
+    def show(self):
+        print(f"{self.name} Balance = {self.balance}")
+        
+Mehmood = bank("Mehmood",100000000)
+Mehmood.deposite(12000)
+Mehmood.widraw(12000)
+
+Ali = bank("Ali",89000)
+Ali.deposite(5000)
+Ali.widraw(10000)
 
 
