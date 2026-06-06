a7acad7e6102e20d99d31594f6cb80f047652653
Mehmood Hassan <mehmood.mh56@gmail.com>
2026-06-06 10:37:16 +0500

    Add result.html file and connect it with index.html, add result function in views.py, add this url in url.py and give it name attribute because otherwise django append url after previous (defaul behaviour), also connect index.html with result.html and result.html with index.html.

--- Python_for_all_levels_course/Django_Project/MyWeb/views.py
+++ Python_for_all_levels_course/Django_Project/MyWeb/views.py
@@ -2,8 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{"key":"I am from Python"})
 
 def downloads(request):
     return HttpResponse('<font face = "sans-serif" color = "navy" size = "7px"><center><h4> No Downloads Available !</h4></center></font>')
+
+def result(request):
+    return render(request, 'result.html')
+
+
+
 
