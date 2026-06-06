bebe4cd44fcadede445b8c81f4f2c3c7134c5d48
Mehmood Hassan <mehmood.mh56@gmail.com>
2026-06-06 17:58:44 +0500

    Make a word counter web app by editing result.html and index.html and view.py and url.py and connect many html files

--- Python_for_all_levels_course/Django_Project/MyWeb/views.py
+++ Python_for_all_levels_course/Django_Project/MyWeb/views.py
@@ -1,5 +1,7 @@
+import operator
 from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def home(request):
     return render(request, 'index.html',{"key":"I am from Python"})
@@ -8,7 +10,23 @@ def downloads(request):
     return HttpResponse('<font face = "sans-serif" color = "navy" size = "7px"><center><h4> No Downloads Available !</h4></center></font>')
 
 def result(request):
-    return render(request, 'result.html')
+    
+    # name = request.GET['name']
+    message = request.GET['message']
+
+    word = message.split()
+    word_count = len(word)
+    dict_words = {}
+
+    for i in word:
+        if i in dict_words:
+            dict_words[i] += 1
+        else:
+            dict_words[i] = 1
+
+    sorted_dict = sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)
+    
+    return render(request, 'result.html', {"message": message, "word_count": word_count, "dict_words": sorted_dict})
 
 
 
